f6823821cc88cfdba330bb825ccf308a654c5938
Etherealxx <64251396+etherealxx@users.noreply.github.com>
2023-08-21 13:35:21 +0700

    Update listtest.py

--- listtest.py
+++ listtest.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import os
+import wget
 
 def list_files_recursive(directory):
     for root, dirs, files in os.walk(directory):
@@ -11,4 +12,9 @@ def list_files_recursive(directory):
             st.write(f"{sub_indent}{file}")
 
 st.title('test')
+os.makedirs('/mount/src/litscrape', exist_ok=True)
+chromedriver_url = "https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/116.0.5845.96/linux64/chromedriver-linux64.zip"
+zip_path = "/mount/src/litscrape/chromedriver.zip"  # Set the desired path on the server
+wget.download(chromedriver_url, zip_path)
+os.system(f"unzip {zip_path} -d {os.path.dirname(chromedriver_path)}")
 list_files_recursive('/mount/src/listtest')
