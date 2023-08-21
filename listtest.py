940bf3bb57af2a5afec6adcb49c71aaaaddfcef3
Etherealxx <64251396+etherealxx@users.noreply.github.com>
2023-08-21 13:40:44 +0700

    Update listtest.py

--- listtest.py
+++ listtest.py
@@ -15,11 +15,11 @@ def list_files_recursive(directory):
 st.title('test')
 os.makedirs('/mount/src/listtest', exist_ok=True)
 chromedriver_url = "https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/116.0.5845.96/linux64/chromedriver-linux64.zip"
-zip_path = "/mount/src/listtest/listtest/chromedriver.zip"  # Set the desired path on the server
+zip_path = "/mount/src/listtest/chromedriver.zip"  # Set the desired path on the server
 # wget.download(chromedriver_url, zip_path)
 # os.system(f"unzip {zip_path} -d {os.path.dirname('/mount/src/listtest')}")
 with zipfile.ZipFile(zip_path, 'r') as zip_ref:
     # Extract all contents to the target directory
-    zip_ref.extractall('/mount/src/listtest/listtest/chromedriver')
+    zip_ref.extractall('/mount/src/listtest/chromedriver')
 
 list_files_recursive('/mount/src/listtest')
